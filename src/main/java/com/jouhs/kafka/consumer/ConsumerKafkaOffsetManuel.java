d7ba77ed659c3f41f6c172322f2bce4df4997b7e
jouhs <jaouad.ouhs@gmail.com>
2024-02-24 11:42:16 +0100

    commit offset manually

---
+++ src/main/java/com/jouhs/kafka/consumer/ConsumerKafkaOffsetManuel.java
@@ -0,0 +1,33 @@
+package com.jouhs.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerKafkaOffsetManuel {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group-id");
+        props.put("enable.auto.commit", "false");
+        // enable.auto.commit is false so no need for this line : props.put("auto.commit.interval.ms","1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("topic-test"));
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset= %d, key = %s, value = %s %n", record.offset(), record.key(), record.value());
+            }
+            // commit offset manually
+            consumer.commitAsync();
+        }
+
+    }
+}
